1695d8b45ba4d0e20770d5f8fa4b5a5c59b3596c
jose <jj2020067148@virtual.upt.pe>
2025-10-05 11:25:14 -0500

    v4.5

--- bd/full/bootstrap_all.sql
+++ bd/full/bootstrap_all.sql
@@ -3,36 +3,36 @@
 -- Asume que estás en la raíz del repo o ajusta rutas relativas según necesidad.
 
 \echo '== Iniciando bootstrap esquema =='
-\i ../schema/01_schema.sql
+\i /bd/schema/01_schema.sql
 
 \echo '== Seed mínimo =='
-\i ../seeds/02_seed_min.sql
+\i /bd/seeds/02_seed_min.sql
 
 \echo '== Catálogo ULTRAVIP =='
-\i ../catalog/03_ultravip.sql
+\i /bd/catalog/03_ultravip.sql
 
 \echo '== Catálogo DUCONDA =='
-\i ../catalog/04_duconda.sql
+\i /bd/catalog/04_duconda.sql
 
 \echo '== Catálogo JCH =='
-\i ../catalog/05_jch.sql
+\i /bd/catalog/05_jch.sql
 
 \echo '== Catálogo REZZIO =='
-\i ../catalog/06_rezzio.sql
+\i /bd/catalog/06_rezzio.sql
 
 \echo '== Catálogo ADVANCE =='
-\i ../catalog/07_advance.sql
+\i /bd/catalog/07_advance.sql
 
 \echo '== Catálogo SONLINK =='
-\i ../catalog/08_sonlink.sql
+\i /bd/catalog/08_sonlink.sql
 
 \echo '== Catálogo WANXIN =='
-\i ../catalog/09_wanxin.sql
+\i /bd/catalog/09_wanxin.sql
 
 \echo '== Catálogo B52 =='
-\i ../catalog/10_b52.sql
+\i /bd/catalog/10_b52.sql
 
 \echo '== Ajuste de secuencias (final) =='
-\i ../maintenance/fix_sequences.sql
+\i /bd/maintenance/fix_sequences.sql
 
 \echo '== Bootstrap completo =='
